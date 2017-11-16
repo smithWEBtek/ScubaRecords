a0570f7923206d16d24cf33552ed584b6759b503
github username <github email address>
2017-11-16 14:30:13 -0500

    Setup new and create action in site controller, setup new in site views

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,8 +1,31 @@
 class SitesController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
+
+  def show
+  end
+
+  def new
+    @site = Site.new
+  end
+
+  def create
+    @site = Site.new(site_params)
+    if @site.save
+      redirect_to site_path(@site)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def site_params
+      params.require(:site).permit(:name, :location)
+    end
+
 
 end
