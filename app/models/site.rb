c38b83741f53f25a8d3fec15df2ae41a7c98d99f
github username <github email address>
2017-11-22 12:39:06 -0500

    Build search scope method in site model

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,6 @@
 class Site < ApplicationRecord
   has_many :records, dependent: :destroy
   has_many :users, through: :records
+
+  # scope :search, ->(site_name) { where("name LIKE ? OR location LIKE ?", "%#{site_name}%", "%#{site_name}%") }
 end
