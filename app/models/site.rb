a1fbbda7e192e6dca8fff790ed6415aa3c3bb6aa
github username <github email address>
2017-11-26 21:58:02 -0500

    Add validations to site model

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,5 +2,9 @@ class Site < ApplicationRecord
   has_many :records, dependent: :destroy
   has_many :users, through: :records
 
+  validates :name, presence: true
+  validates :name, uniqueness: true 
+  validates :location, presence: true
+
   # scope :search, ->(site_name) { where("name LIKE ? OR location LIKE ?", "%#{site_name}%", "%#{site_name}%") }
 end
