d1b61e47a64ce04a681498a7a13785a56efb17c1
github username <github email address>
2017-11-28 12:42:19 -0500

    Added validations to record

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,11 @@
 class Record < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :site
+
+  # validates :date, presence: true
+  # validates :dive_time, presence: true
+  # validates :max_depth, presence: true
+  # validates :water_temperature, presence: true
+  # validates :activity, presence: true
+  # validates :notes, presence: true
 end
