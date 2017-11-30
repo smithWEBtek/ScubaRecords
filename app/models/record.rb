c8ea4009257ad861d0b03d957bc487d8fabc920c
github username <github email address>
2017-11-29 22:08:03 -0500

    Add id uniqueness validation to record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,10 +2,11 @@ class Record < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :site
 
-  # validates :date, presence: true
-  # validates :dive_time, presence: true
-  # validates :max_depth, presence: true
-  # validates :water_temperature, presence: true
-  # validates :activity, presence: true
-  # validates :notes, presence: true
+  validates :id, uniqueness: true
+  validates :date, presence: true
+  validates :dive_time, presence: true
+  validates :max_depth, presence: true
+  validates :water_temperature, presence: true
+  validates :activity, presence: true
+  validates :notes, presence: true
 end
