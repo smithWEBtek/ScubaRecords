acdce7bba2d18042481b1dc4e5950a4e5c01ce24
github username <github email address>
2017-12-18 17:01:08 -0500

    ignore devise.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,11 @@ class User < ApplicationRecord
   has_many :records
   has_many :sites, through: :records
 
-  validates :username, presence: true
-  validates :username, uniqueness: true
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable
 
-  def self.from_omniauth(access_token)
-  data = access_token.info
-  user = User.where(:email => data["email"]).first
-
-    unless user
-      password = Devise.friendly_token[0,20]
-      user = User.create(username: data["name"], email: data["email"],
-        password: password, password_confirmation: password
-      )
-    end
-    user
-  end
-
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
