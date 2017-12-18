918c0de6e053a1143e9accfd90cb2824d15bec9e
github username <github email address>
2017-12-18 17:24:45 -0500

    Add Omniauth with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,25 @@ class User < ApplicationRecord
   has_many :records
   has_many :sites, through: :records
 
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
-  validates :username, presence: true
-  validates :username, uniqueness: true
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+      password = Devise.friendly_token[0,20]
+      user = User.create(username: data["name"], email: data["email"],
+        password: password, password_confirmation: password
+      )
+    end
+    user
+  end
+
 end
